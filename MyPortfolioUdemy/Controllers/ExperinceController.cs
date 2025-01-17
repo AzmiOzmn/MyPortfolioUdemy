135d4a36bdcb5a5b619ae9ccf47f04497053b2db
AzmiOzmn <azmiozmen5234@gmail.com>
2025-01-17 23:32:20 +0300

    Update

---
+++ MyPortfolioUdemy/Controllers/ExperinceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+	public class ExperinceController : Controller
+	{
+		MyPortfolioContext db = new MyPortfolioContext();
+		public IActionResult ExperienceList()
+		{
+			var values = db.Experiences.ToList();
+			return View(values);
+		}
+
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public IActionResult CreateExperience(Experience model)
+		{
+			db.Experiences.Add(model);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+	}
+}
